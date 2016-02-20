c804211c39411eb155e4c7a516609afb2f481c10
Jens Jaeger <jaegerj204@googlemail.com>
2016-02-20 13:01:36 +0100

    create observable from async IO

---
+++ src/LogFileService/Model/DataBlock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LogFileService.Model
+{
+    public class DataBlock
+    {
+        public DataBlock(int size)
+        {
+            Data = new char[size];
+        }
+
+        public char[] Data;
+
+        public long StartPosition
+        {
+            get
+            {
+                if (PreviousBlock == null)
+                    return 0;
+                return PreviousBlock.EndPosition + 1;
+            }
+        }
+
+        public int Length => Data.Length;
+
+        public long EndPosition => StartPosition + Length;
+
+        public void ResizeData(int newLength)
+        {
+            Array.Resize(ref Data, newLength);
+        }
+
+        public DataBlock PreviousBlock { get; set; }
+    }
+}
---
+++ src/LogFileService/Service/BlockReader.cs
@@ -0,0 +1,56 @@
+﻿using LogFileService.Model;
+using System;
+using System.IO;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace LogFileService.Service
+{
+    public class BlockReader
+    {
+        const int BufferSize = 8*1024;
+
+        public IObservable<DataBlock> ReadBlocksFromFile(string filename)
+        {
+            var obs = Observable.Create<DataBlock>(async (subject, token) =>
+            {
+                try
+                {
+                    using (var stream = File.Open(filename, FileMode.Open, FileAccess.Read))
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            DataBlock prev = null;
+                            while (!reader.EndOfStream)
+                            {
+                                if (token.IsCancellationRequested)
+                                    break;
+
+                                var block = new DataBlock(BufferSize);
+                                block.PreviousBlock = prev;
+                                block = await ReadBlockAsync(reader, block);
+                                subject.OnNext(block);
+                                prev = block;
+                            }
+                            subject.OnCompleted();
+                        }
+                    }
+                } catch (Exception ex)
+                {
+                    subject.OnError(ex);
+                }
+            });
+            return obs;
+        }
+
+        private async Task<DataBlock> ReadBlockAsync(StreamReader reader, DataBlock target)
+        {
+            var readBytes = await reader.ReadAsync(target.Data, 0, BufferSize);
+            if (readBytes != BufferSize)
+            {
+                target.ResizeData(readBytes);
+            }
+            return target;
+        }
+    }
+}
