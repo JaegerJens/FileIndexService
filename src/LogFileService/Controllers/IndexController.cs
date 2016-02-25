c10ff0591006e160abed0284f070369f35d8b574
Jens Jaeger <jaegerj204@googlemail.com>
2016-02-25 13:19:33 +0100

    combine characters to string and separate text lines

--- src/LogFileService/Controllers/IndexController.cs
+++ src/LogFileService/Controllers/IndexController.cs
@@ -2,6 +2,7 @@
 using LogFileService.Service;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
+using System;
 
 namespace LogFileService.Controllers
 {
@@ -10,12 +11,15 @@ namespace LogFileService.Controllers
     {
         public async Task<IActionResult> Index()
         {
+            var sep = new LineSeparator();
             var reader = new BlockReader();
             var blockList = reader.ReadBlocksFromFile(@"wwwroot\Data\testdata.txt");
-            var data = await blockList
-                .SelectMany(f => f.Data)
-                .ToList()
-                .Select(t => Json(t));
+            var d1 = blockList
+                .SelectMany(f => f.Data);
+            IObservable<string> dl = d1.SelectMany(sep.Separate);
+            var d2 = dl.ToList();
+            var d3 = d2.Select(t => Json(t));
+            var data = await d3;
             return data;
         }
     }
--- src/LogFileService/Service/LineSeparator.cs
+++ src/LogFileService/Service/LineSeparator.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace LogFileService.Service
 {
@@ -8,61 +7,27 @@ namespace LogFileService.Service
         public const char CarriageReturn = '\r';
         public const char LineFeed = '\n';
 
-        public IEnumerable<string> SeparateLines(IEnumerable<char[]> input)
+        private string _buffer;
+
+        public LineSeparator()
         {
-            return SeparateLines(input.Select(d => new string(d)));
+            _buffer = null;
         }
 
-        public IEnumerable<string> SeparateLines(IEnumerable<string> input)
+        public IEnumerable<string> Separate(char inputChar)
         {
-            string resume = null;
-            foreach(var currentInput in input)
+            if (inputChar == CarriageReturn)
             {
-                // work only with variable inp
-                var inp = currentInput;
-
-                // if resume has left overs from last line
-                // then concat inp with resume
-                if (resume != null)
-                {
-                    inp = resume + inp;
-                    resume = null;
-                }
-
-                while(inp.Length > 0)
-                {
-                    // find end of line
-                    var pos = inp.IndexOf(LineFeed);
-
-                    // if current inp has no end of line
-                    // then remember left over in resume
-                    // and continue with next line
-                    if (pos == -1)
-                    {
-                        resume = inp;
-                        inp = string.Empty;
-                        continue;
-                    }
-
-                    // find windows end of line: \r\n
-                    bool hasWindowsEndOfLine = false;
-                    if (pos > 0)
-                    {
-                        hasWindowsEndOfLine = (inp[pos - 1] == CarriageReturn);
-                    }
-
-                    // yield return everything until end of line
-                    if (hasWindowsEndOfLine)
-                    {
-                        yield return inp.Substring(0, pos - 1);
-                    }
-                    else
-                    {
-                        yield return inp.Substring(0, pos);
-                    }
-                    inp = inp.Substring(pos + 1);
-                }
+                yield return _buffer;
+                _buffer = null;
+                yield break;
             }
+            if (inputChar == LineFeed)
+            {
+                yield break;
+            }
+            _buffer = _buffer + inputChar;
+            yield break;
         }
     }
 }
